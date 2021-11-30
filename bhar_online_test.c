290085aeb4892738bb7fe9e3a2ccc885c1ca5128
akashilanthendral@gmail.com <akashilanthendral@gmail.com>
2021-11-30 16:30:53 +0530

    Bhargavi interview question

---
+++ bhar_online_test.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+// #define x 5+2
+
+int main()
+{
+    int s, n, m;
+    scanf("%d %d %d", &s, &n, &m);
+    if(n < m){
+        printf("not possible");
+    }else if(n == m){
+        printf("%d", s);
+    }else{
+        if(s%(n/m) == 0){
+            printf("%d", s/(n/m));
+        }else{
+            printf("%d", (s/(n/m))+1);
+        }
+    }
+
+    return 0;
+}
