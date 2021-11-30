459c194b53790f3266b642ff695257a2873acd7f
akashilanthendral@gmail.com <akashilanthendral@gmail.com>
2021-11-30 17:45:53 +0530

    commit from master

---
+++ bhar_online_test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int s, n, m;
+    scanf("%d %d %d", &s, &n, &m);
+    if(n < m){
+        printf("not possible");
+    }else if(n == m){
+        printf("%d", s);
+    }else{
+        if(s%(n/m) == 0){
+            printf("%d", s/(n/m));
+        }else{
+
+            printf("%d", (s/(n/m))+1);
+        }
+    }
+    //this line added temporarily...
+    return 0;
+}
